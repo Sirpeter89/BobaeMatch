8d9fd297eee98db20ce0a253367633e04b46920f
Justin <just2rules@gmail.com>
2021-06-30 20:20:45 -0700

    finished custom made carousel component

--- react-app/src/components/SplashPage/Home.js
+++ react-app/src/components/SplashPage/Home.js
@@ -4,29 +4,42 @@ import './Home.css'
 
 export default function HomePage(){
     const [transition, setTransition] = useState("disabled")
-    const [blue, setBlue] = useState("https://images.unsplash.com/photo-1529474944862-bf4949bd2f1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1650&q=80")
-    const [brown, setBrown] = useState("https://images.unsplash.com/photo-1613018274498-7fbbaa26d5d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80")
-    const [index, setIndex] = useState(0)
+
+    const imageList = [
+        "https://images.unsplash.com/photo-1529474944862-bf4949bd2f1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1650&q=80", //blue boba image 0
+        "https://images.unsplash.com/photo-1613018274498-7fbbaa26d5d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80", //brown boba image 1
+        "https://images.unsplash.com/photo-1600505799290-e32386140a02?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80", //lovers image 2
+        "https://images.unsplash.com/reserve/Af0sF2OS5S5gatqrKzVP_Silhoutte.jpg?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+    ]
+
+    const [one, setOne] = useState(imageList[0])
+    const [two, setTwo] = useState(imageList[1])
+
+    const [index, setIndex] = useState(1)
 
     useEffect(async ()=>{
         const change = await setTimeout(()=>{
             if (transition === "disabled"){
                 setTransition("enabled")
-            } else {
+            }
+        }, 2000)
+        const delay = setTimeout(()=>{
+            if (transition === "enabled"){
+                if(index === imageList.length-1){ //if we're at the last image
+                    setOne(imageList[index])
+                    setTwo(imageList[0])
+                    setIndex(0)
+                } else {
+                    setOne(imageList[index])
+                    setTwo(imageList[index+1])
+                    setIndex(index+1)
+                }
                 setTransition("disabled")
             }
-        }, 3000)
-        if (index === 0){
-            setBlue(brown)
-            setBrown(blue)
-            setIndex(1)
-        } else {
-            setBlue(blue)
-            setBrown(brown)
-            setIndex(0)
-        }
+        },4000)
         return () => {
             clearTimeout(change);
+            clearTimeout(delay);
         }
     }, [transition])
 
@@ -37,8 +50,8 @@ export default function HomePage(){
         </div>
         <div className='centerCarousel'>
             <div className='carouselContainer'>
-                <img className={`image ${transition}`} src={blue}></img>
-                <img className={`nextImage ${transition}`} src={brown}></img>
+                <img className={`image ${transition}`} src={one}></img>
+                <img className={`nextImage ${transition}`} src={two}></img>
             </div>
         </div>
         </>
