f067c9c7a1f681f25d53b0dbca414e602a3fedc4
Justin <just2rules@gmail.com>
2021-07-02 10:25:46 -0700

    added data and styled profile page

--- react-app/src/components/ProfilePage/ProfilePage.js
+++ react-app/src/components/ProfilePage/ProfilePage.js
@@ -12,20 +12,38 @@ export default function ProfilePage(){
 
     return(
         <>
-            <div className = "userName">
-                User Name: <b>{user.username}</b>
+            <div className='backgroundImageContProfile'>
+                <img className='backgroundImageProfile' src='https://images.unsplash.com/photo-1555050551-82f8d95a0614?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80' />
             </div>
-            <div className = "name">
-                Name: <b>{user.firstname} {user.lastname}</b>
+            <div className="profileDetailsCont">
+                <div className="leftContainer">
+                    <div className="detailsTitle">
+                        Information:
+                    </div>
+                    <div className="profileDetails">
+                        <div className = "userName">
+                            User Name: <b>{user.username}</b>
+                        </div>
+                        <div className = "name">
+                            Name: <b>{user.firstname} {user.lastname}</b>
+                        </div>
+                        <div className = "cityZip">
+                            Location: <b>{user.city}, {user.zipcode}</b>
+                        </div>
+                        <div className = "age">
+                            Age: <b>{user.age}</b>
+                        </div>
+                        <div className = "height">
+                            Height: <b>{feet}'{inches}"</b>
+                        </div>
+                    </div>
+                </div>
+                <div className="rightContainer">
+                    Preferences:
+                </div>
             </div>
-            <div className = "cityZip">
-                Location: {user.city}, {user.zipcode}
-            </div>
-            <div className = "age">
-                Age: {user.age}
-            </div>
-            <div className = "height">
-                Height: {feet}'{inches}"
+            <div className="profileImage">
+                <img className="pImg" src={user.profileImage} />
             </div>
         </>
     )
