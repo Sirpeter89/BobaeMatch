cddbed5bc17fd80936efee82c332c707bdf18b3a
Justin <just2rules@gmail.com>
2021-08-09 15:50:42 -0700

    styling chat box, need to switch branch

--- react-app/src/components/MatchPage/ChatComponent/ChatComponent.js
+++ react-app/src/components/MatchPage/ChatComponent/ChatComponent.js
@@ -10,13 +10,11 @@ export default function ChatComponent(props){
     const [chatInput, setChatInput] = useState("");
 
     const [userToTalkWith, setuserToTalkWith] = useState("");
+    const [userToTalkWithName, setuserToTalkWithName] = useState("")
 
     const user = useSelector(state => state.session.user)
 
     useEffect(()=>{
-        console.log("ISS", props.matchData)
-        console.log(userToTalkWith)
-
         socket = io();
         socket.emit("join", userToTalkWith);
 
@@ -38,43 +36,59 @@ export default function ChatComponent(props){
         setChatInput(e.target.value)
     };
 
-    const joinChat = (matchId) => {
+    const joinChat = (matchId, username) => {
         setuserToTalkWith(matchId)
+        setuserToTalkWithName(username)
 
     }
 
     const sendChat = (e) => {
         e.preventDefault()
         // emit a message
-        console.log("WHAT", chatInput)
         socket.emit("chat", { user: user.username, msg: chatInput }, userToTalkWith);
         // clear the input field after the message is sent
         setChatInput("")
     }
 
-    return(
-        <div className="chat-container">
-            <div className="names-chat-container">
-                {props.matchData.map((person)=>(
-                    <div onClick={()=>joinChat(`${person[2]}`)} className="user-chat-container">
-                        {person[0].firstname}
-                    </div>
-                ))}
+    let pickUser;
+        if (!userToTalkWithName){
+            pickUser =
+            <div className="pick-input">
+                Select a Bobae to chat with!
             </div>
+        } else {
+            pickUser =
             <div className="chat-area">
-                <div>
+                <div className="chat-title">
+                    Chatting With: <b>{userToTalkWithName}</b>
+                </div>
+                <div className="chat-view">
                     {messages.map((message, ind) => (
                         <div key={ind}>{`${message.user}: ${message.msg}`}</div>
                     ))}
                 </div>
-                <form onSubmit={sendChat}>
+                <form onSubmit={sendChat} className="chat-input">
                     <input
                         value={chatInput}
                         onChange={updateChatInput}
+                        className="input-element"
                     />
-                    <button type="submit">Send</button>
+                    <button className="chat-submit" type="submit">Send</button>
                 </form>
             </div>
+        }
+
+
+    return(
+        <div className="chat-container">
+            <div className="names-chat-container">
+                {props.matchData.map((person)=>(
+                    <div onClick={()=>joinChat(`${person[2]}`, `${person[0].username}`)} className="user-chat-container">
+                        {person[0].username}
+                    </div>
+                ))}
+            </div>
+            {pickUser}
         </div>
     )
 }
