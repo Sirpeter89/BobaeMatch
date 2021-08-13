b2cb4b6c9b594f4eef3709def2eaae6c4c22b13f
Justin <just2rules@gmail.com>
2021-08-12 19:26:29 -0700

    reordered matches page to show newest match first

--- react-app/src/components/MatchPage/MatchPage.js
+++ react-app/src/components/MatchPage/MatchPage.js
@@ -144,7 +144,7 @@ export default function MatchPage(){
                                 </div>
 
                         </div>
-                    ))}
+                    )).reverse()}
 
                     </>
             matchBox =
